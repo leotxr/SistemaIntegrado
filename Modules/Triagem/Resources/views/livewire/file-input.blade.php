0fc14e998dbb60cb849f45ec66ea894066113c75
leotxr <leonardof15@hotmail.com>
2023-04-26 14:11:41 -0300

    file input configurado p/ camera

--- Modules/Triagem/Resources/views/livewire/file-input.blade.php
+++ Modules/Triagem/Resources/views/livewire/file-input.blade.php
@@ -14,7 +14,7 @@
             <div>
                 <input type="file" wire:model="photo"
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                    aria-describedby="arquivo_help" accept="image/*;capture=camera">
+                    aria-describedby="arquivo_help" accept="image/*;capture=camera" capture>
             </div>
 
             <div class="mx-2">
