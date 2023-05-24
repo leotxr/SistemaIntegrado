7696736292cd46bf68b851a94672e2ff880dd4cb
leotxr <leonardof15@hotmail.com>
2023-05-24 07:19:27 -0300

    option setada sem padrao

--- Modules/Triagem/Resources/views/livewire/file-input.blade.php
+++ Modules/Triagem/Resources/views/livewire/file-input.blade.php
@@ -20,6 +20,7 @@
             <div class="mx-2">
                 <select wire:model='type' required
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected >Selecione Tipo</option>
                     @foreach($file_type as $type)
                     <option value="{{$type->id}}">{{$type->name}}</option>
                     @endforeach
