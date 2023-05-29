6cd4d6edfd33f15a2c4da92fe3314a945d8608e4
leotxr <leonardof15@hotmail.com>
2023-05-29 10:49:52 -0300

    tamanho do grafico fixado

--- Modules/Triagem/Resources/views/livewire/dashboard/charts/chart-triagens.blade.php
+++ Modules/Triagem/Resources/views/livewire/dashboard/charts/chart-triagens.blade.php
@@ -1,9 +1,9 @@
 <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8 py-6 sm:py-6" wire:poll.10000ms>
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <div class="bg-white dark:bg-gray-900">
+        <div class="bg-white dark:bg-gray-900 h-48">
             <livewire:livewire-line-chart :line-chart-model="$lineChartRM" />
         </div>
-        <div class="bg-white dark:bg-gray-900">
+        <div class="bg-white dark:bg-gray-900 h-48">
             <livewire:livewire-line-chart :line-chart-model="$lineChartRM" />
         </div>
     </div>
