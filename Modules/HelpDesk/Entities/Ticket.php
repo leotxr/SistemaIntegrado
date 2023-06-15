055a19cb1fca54ff820d558b7e6519d50787fcc1
leotxr <leonardof15@hotmail.com>
2023-06-15 09:32:44 -0300

    alteracao caminho ticket messagens

--- Modules/HelpDesk/Entities/Ticket.php
+++ Modules/HelpDesk/Entities/Ticket.php
@@ -4,7 +4,7 @@ namespace Modules\HelpDesk\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Modules\Helpdesk\Entities\TicketMessage;
+use Modules\HelpDesk\Entities\TicketMessage;
 
 class Ticket extends Model
 {
