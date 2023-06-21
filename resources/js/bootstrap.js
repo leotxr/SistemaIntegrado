ae14ff02b27e4f9749300b3bcab7bc8d49b4e1bc
leotxr <leonardof15@hotmail.com>
2023-06-21 08:35:56 -0300

    echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,18 +7,7 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
- import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
  import Pusher from 'pusher-js';
  window.Pusher = Pusher;
  window.Echo = new Echo({
@@ -31,3 +20,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
      forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
      enabledTransports: ['ws', 'wss'],
  });
+ 
+
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+ 
