e724a9555cd800113589beb9ef46c71e18c7645d
leotxr <leonardof15@hotmail.com>
2023-06-21 15:22:57 -0300

    ticket stat tma

--- Modules/HelpDesk/Http/Livewire/Dashboard/TicketStats.php
+++ Modules/HelpDesk/Http/Livewire/Dashboard/TicketStats.php
@@ -2,6 +2,7 @@
 
 namespace Modules\HelpDesk\Http\Livewire\Dashboard;
 
+use DateTime;
 use Livewire\Component;
 use Modules\HelpDesk\Entities\Ticket;
 
@@ -11,8 +12,11 @@ class TicketStats extends Component
 
     public function render()
     {
-        $tma_hoje =  Ticket::whereBetween('ticket_open', [date('Y-m-d 00:00:00'), date('Y-m-d 23:59:59:59')])->avg('total_ticket');
         
+
+        $tma =  Ticket::whereBetween('ticket_open', [date('Y-m-d', strtotime("-1 days")), date('Y-m-d')])->avg('total_ticket');
+        $tma_hoje = gmdate("H:i:s", $tma);
+        //$tma_hoje = $tma->format("%H:%I:%S");
         return view('helpdesk::livewire.dashboard.ticket-stats', 
     [   
         'tma_hoje' => $tma_hoje
