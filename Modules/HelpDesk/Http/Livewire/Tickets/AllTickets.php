9731b8ad7ffa8d36ba0fb306a475d505ccf010e1
leotxr <leonardof15@hotmail.com>
2023-06-22 13:46:07 -0300

    tela todos os chamados

--- Modules/HelpDesk/Http/Livewire/Tickets/AllTickets.php
+++ Modules/HelpDesk/Http/Livewire/Tickets/AllTickets.php
@@ -3,11 +3,32 @@
 namespace Modules\HelpDesk\Http\Livewire\Tickets;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Modules\HelpDesk\Entities\Ticket;
 
 class AllTickets extends Component
 {
+    use WithPagination;
+
+    public $sortField = 'tickets.id';
+    public $sortDirection = 'desc';
+    public $search = '';
+
+    public function sortBy($field)
+    {
+
+        $this->sortDirection = $this->sortField === $field
+            ? $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc'
+            : 'asc';
+
+        $this->sortField = $field;
+    }
+
     public function render()
     {
-        return view('helpdesk::livewire.tickets.all-tickets');
+        return view('helpdesk::livewire.tickets.all-tickets',
+    [
+        'tickets' => Ticket::search($this->sortField, $this->search)->orderBy($this->sortField, $this->sortDirection)->paginate(10)
+    ]);
     }
 }
--- Modules/HelpDesk/Resources/views/livewire/tickets/all-tickets.blade.php
+++ Modules/HelpDesk/Resources/views/livewire/tickets/all-tickets.blade.php
@@ -1,3 +1,65 @@
 <div>
-    <h3>The <code>AllTickets</code> livewire component is loaded from the <code>HelpDesk</code> module.</h3>
+
+
+    <div>
+        <div class="mt-4">
+            <x-text-input type="text" wire:model='search' placeholder="Buscar solicitações...">
+            </x-text-input>
+            <x-input-error class="mt-2" :messages="$errors->get('exam.exam')" />
+        </div>
+    </div>
+    {{$tickets->links()}}
+    <x-table>
+        <x-slot name="head">
+            <x-table.heading sortable wire:click="sortBy('tickets.id')"
+                :direction="$sortField === 'tickets.id' ? $sortDirection : null">Protocolo</x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.ticket_open')"
+                :direction="$sortField === 'ticket.ticket_open' ? $sortDirection : null">Data</x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.title')"
+                :direction="$sortField === 'tickets.title' ? $sortDirection : null">Assunto</x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.requester_id')"
+                :direction="$sortField === 'tickets.requester_id' ? $sortDirection : null">Solicitante
+            </x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.category_id')"
+                :direction="$sortField === 'ticket.category_id' ? $sortDirection : null">Categoria</x-table.heading>
+
+            <x-table.heading sortable wire:click="sortBy('tickets.sub_category_id')"
+                :direction="$sortField === 'tickets.sub_category_id' ? $sortDirection : null">Sub-Categoria
+            </x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.user_id')"
+                :direction="$sortField === 'tickets.user_id' ? $sortDirection : null">Atendente
+            </x-table.heading>
+            <x-table.heading sortable wire:click="sortBy('tickets.status_id')"
+                :direction="$sortField === 'ticket.status_id' ? $sortDirection : null">Status</x-table.heading>
+
+        </x-slot>
+
+        <x-slot name="body">
+            @foreach ($tickets as $ticket)
+            @php
+            $solicitante = $ticket->find($ticket->id)->TicketRequester;
+            $atendente = $ticket->find($ticket->id)->TicketUser;
+            $status = $ticket->find($ticket->id)->TicketStatus;
+            $categoria = $ticket->find($ticket->id)->TicketCategory;
+            $subcategoria = $ticket->find($ticket->id)->TicketSubCategory;
+            @endphp
+            <x-table.row>
+                <x-table.cell>{{
+                    $ticket->id }}</x-table.cell>
+                <x-table.cell>{{ $ticket->ticket_open ?? '?' }}</x-table.cell>
+                <x-table.cell>{{ $ticket->title ?? '?' }}</x-table.cell>
+                <x-table.cell>{{ $solicitante->name }}</x-table.cell>
+                <x-table.cell>{{ $categoria->name ?? '?' }}</x-table.cell>
+                <x-table.cell>{{ $subcategoria->name ?? '?' }}</x-table.cell>
+                <x-table.cell>{{ $atendente->name ?? 'Sem atendente' }}</x-table.cell>
+                <x-table.cell>{{ $status->name ?? '?'
+                    }}
+                </x-table.cell>
+
+            </x-table.row>
+            @endforeach
+        </x-slot>
+    </x-table>
+    {{$tickets->links()}}
+
 </div>
