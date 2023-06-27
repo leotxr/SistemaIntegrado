2c0ec1eefd5b255d2e9b4e90e4275a76ee281762
leotxr <leonardof15@hotmail.com>
2023-06-27 13:21:40 -0300

    notify email

--- Modules/HelpDesk/Notifications/NotifyTicketCreated.php
+++ Modules/HelpDesk/Notifications/NotifyTicketCreated.php
@@ -34,7 +34,7 @@ class NotifyTicketCreated extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['mail','database'];
     }
 
     /**
