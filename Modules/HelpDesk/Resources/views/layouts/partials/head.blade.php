ee4073303fb336425c3d63de1d70d445d17ae29c
leotxr <leonardof15@hotmail.com>
2023-06-28 17:23:58 -0300

    head modulo helpdesk

--- Modules/HelpDesk/Resources/views/layouts/partials/head.blade.php
+++ Modules/HelpDesk/Resources/views/layouts/partials/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="mobile-web-app-capable" content="yes">
-<title>{{ config('app.name', 'sigma') }}</title>
+<title>HelpDesk | SIGMA - Ultrimagem</title>
 
 <!-- Fonts -->
 <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
