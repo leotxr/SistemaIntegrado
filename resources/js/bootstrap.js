9e160b3fb256141acf8be42f89f69efb685db300
leotxr <leonardof15@hotmail.com>
2023-06-29 17:15:47 -0300

    teste wss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,7 +15,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wssHost: 'intranet.ultrimagemuba.com.br:8083',
+    wssHost: 'intranet.ultrimagemuba.com.br',
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
