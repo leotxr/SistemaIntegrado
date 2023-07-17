49958cf7d8094a5a08a4b6c09282a7a643e77b34
leotxr <leonardof15@hotmail.com>
2023-07-17 16:48:58 -0300

    alteracao fila de monitoramento

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,35 @@
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                    
+                    <div x-data="{
+        content: 'Lorem ipsum dolor sit amet'
+    }" class="bg-gray-200 border border-gray-900">
+                        <alpine-editor x-model="content" data-h1-classes="text-xl">
+                            <div data-type="menu">
+                                <button type="button" data-command="strong" data-active-class="bg-blue-400"
+                                    class="bg-gray-500">
+                                    Bold
+                                </button>
+                                <button type="button" data-command="em" data-active-class="bg-blue-400"
+                                    class="bg-gray-500">
+                                    Emphasize
+                                </button>
+                                <button type="button" data-command="code" data-active-class="bg-blue-400"
+                                    class="bg-gray-500">
+                                    Code
+                                </button>
+                                <button type="button" data-command="heading" data-level="1"
+                                    data-active-class="bg-blue-400" class="bg-gray-500">
+                                    H1
+                                </button>
+                            </div>
+
+                            <div data-type="editor" class="p-2">
+                                as
+                            </div>
+                        </alpine-editor>
+                    </div>
+
                 </div>
             </div>
         </div>
