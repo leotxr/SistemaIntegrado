271794ce9daa16a10f9dc889e62a3c4aeaf3472e
leotxr <leonardof15@hotmail.com>
2023-08-31 09:44:28 -0300

    modulo orçamento

---
+++ Modules/Orcamento/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('orcamento::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('orcamento.name') !!}
+    </p>
+@endsection
