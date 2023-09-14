40d216d3ea6e71795188efde872d95bd6311ccac
leotxr <leonardof15@hotmail.com>
2023-09-14 07:54:48 -0300

    condicao count

--- Modules/Orcamento/Resources/views/livewire/dashboard/top-users.blade.php
+++ Modules/Orcamento/Resources/views/livewire/dashboard/top-users.blade.php
@@ -8,12 +8,14 @@
         </x-slot>
         <x-slot name="body">
             @foreach($users as $user)
+            @if($user->relBudgets->count() > 0)
             <x-table.row>
                 <x-table.cell>{{$user->name}}</x-table.cell>
                 <x-table.cell>{{$user->relBudgets->where('budget_status_id', 3)->count()}}</x-table.cell>
                 <x-table.cell>{{$user->relBudgets->where('budget_status_id', 2)->count()}}</x-table.cell>
                 <x-table.cell>{{$user->relBudgets->count()}}</x-table.cell>
             </x-table.row>
+            @endif
             @endforeach
         </x-slot>
     </x-table>
