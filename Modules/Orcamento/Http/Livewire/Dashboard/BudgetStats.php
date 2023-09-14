3c64219d1973555a3f23318db51e918d8e58dbf0
leotxr <leonardof15@hotmail.com>
2023-09-14 10:39:48 -0300

    stats

--- Modules/Orcamento/Http/Livewire/Dashboard/BudgetStats.php
+++ Modules/Orcamento/Http/Livewire/Dashboard/BudgetStats.php
@@ -3,9 +3,17 @@
 namespace Modules\Orcamento\Http\Livewire\Dashboard;
 
 use Livewire\Component;
+use Modules\Orcamento\Entities\Budget;
 
 class BudgetStats extends Component
 {
+    public $count_budgets;
+
+    public function mount()
+    {
+        $this->count_budgets = Budget::whereMonth('created_at', date('m'))->get();
+    }
+    
     public function render()
     {
         return view('orcamento::livewire.dashboard.budget-stats');
--- Modules/Orcamento/Resources/views/livewire/dashboard/budget-stats.blade.php
+++ Modules/Orcamento/Resources/views/livewire/dashboard/budget-stats.blade.php
@@ -2,23 +2,43 @@
     <div class="grid w-full grid-cols-2 gap-1 px-2 py-4 bg-white divide-x-2 dark:bg-gray-800 sm:grid-cols-6 divide-solid">
         <div class="col-span-1 text-center sm:col-span-1">
             <div>
-                <span class="text-3xl font-bold text-gray-700 dark:text-gray-50">48</span>
+                <span class="text-3xl font-bold text-gray-700 dark:text-gray-50">{{$count_budgets->count()}}</span>
             </div>
             <div>
                 <span class="text-sm text-gray-500 font-regular dark:text-gray-200">Orçamentos</span>
             </div>
         </div>
         <div class="col-span-1 text-center sm:col-span-1">
-            teste
+            <div>
+                <span class="text-3xl font-bold text-gray-700 dark:text-gray-50">{{$count_budgets->where('budget_status_id', 3)->count()}}</span>
+            </div>
+            <div>
+                <span class="text-sm text-gray-500 font-regular dark:text-gray-200">Agendados</span>
+            </div>
         </div>
         <div class="col-span-2 text-center sm:col-span-1">
-            teste
+            <div>
+                <span class="text-3xl font-bold text-gray-700 dark:text-gray-50">{{$count_budgets->where('budget_status_id', 2)->count()}}</span>
+            </div>
+            <div>
+                <span class="text-sm text-gray-500 font-regular dark:text-gray-200">Não Agendados</span>
+            </div>
         </div>
         <div class="col-span-1 text-center sm:col-span-1">
-            teste
+            <div>
+                <span class="text-3xl font-bold text-gray-700 dark:text-gray-50">{{$count_budgets->where('budget_status_id', 1)->count()}}</span>
+            </div>
+            <div>
+                <span class="text-sm text-gray-500 font-regular dark:text-gray-200">Pendentes</span>
+            </div>
         </div>
-        <div class="col-span-1 text-center sm:col-span-1">
-            teste
+        <div class="flex col-span-1 space-x-2 text-center sm:col-span-2">
+            <div>
+                
+            </div>
+            <div>
+                
+            </div>
         </div>
     </div>
 </div>
