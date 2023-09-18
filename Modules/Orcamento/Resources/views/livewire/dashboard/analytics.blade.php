ab3ece1808300f8c895f9d9cc3a6f88669785fe0
leotxr <leonardof15@hotmail.com>
2023-09-18 08:48:50 -0300

    col span chart

--- Modules/Orcamento/Resources/views/livewire/dashboard/analytics.blade.php
+++ Modules/Orcamento/Resources/views/livewire/dashboard/analytics.blade.php
@@ -33,10 +33,10 @@
         <div class="col-span-1 p-2 sm:col-span-6">
             @livewire('orcamento::dashboard.top-users')
         </div>
-        <div class="col-span-1 p-2 bg-white sm:col-span-3 dark:bg-gray-800">
+        <div class="col-span-1 p-2 bg-white sm:col-span-6 dark:bg-gray-800">
             @livewire('orcamento::dashboard.budget-by-month')
         </div>
-        <div class="col-span-1 p-2 bg-white sm:col-span-5 dark:bg-gray-800">
+        <div class="col-span-1 p-2 bg-white sm:col-span-6 dark:bg-gray-800">
             @livewire('orcamento::dashboard.budget-by-day')
         </div>
 
