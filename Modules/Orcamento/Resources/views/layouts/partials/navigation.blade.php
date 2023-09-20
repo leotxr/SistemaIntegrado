2e50f7d69108e7b18a70ec48c86e00afee31082f
leotxr <leonardof15@hotmail.com>
2023-09-20 13:40:00 -0300

    indigo to blue

--- Modules/Orcamento/Resources/views/layouts/partials/navigation.blade.php
+++ Modules/Orcamento/Resources/views/layouts/partials/navigation.blade.php
@@ -14,20 +14,77 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    @can(['editar autorizacao', 'excluir autorizacao'])
-                    <x-nav-link style="cursor: pointer" data-drawer-target="drawer-navigation"
-                        data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
-                        {{ __('Relatórios') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('orcamento.dashboard')" :active="request()->routeIs('orcamento.dashboard')">
-                        {{ __('Painel') }}
-                    </x-nav-link>
-                    @endcan
-                    <x-nav-link :href="route('orcamento.index')" :active="request()->routeIs('orcamento.index')">
-                        {{ __('Orçamentos') }}
-                    </x-nav-link>
 
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                <div>Orçamentos</div>
+
+                                <div class="ml-1">
+                                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('orcamento.index')"
+                                :active="request()->routeIs('orcamento.index')">
+                                {{ __('Orçamentos') }}
+                            </x-dropdown-link>
+                            @can(['editar autorizacao', 'excluir autorizacao'])
+                            <x-dropdown-link :href="route('orcamento.dashboard')"
+                                :active="request()->routeIs('orcamento.dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link style="cursor: pointer" data-drawer-target="drawer-navigation"
+                                data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+                                {{ __('Relatórios') }}
+                            </x-dropdown-link>
+                            @endcan
+
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                <div>Agenda</div>
+
+                                <div class="ml-1">
+                                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('orcamento.index')"
+                                :active="request()->routeIs('orcamento.index')">
+                                {{ __('Encaixes') }}
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('orcamento.dashboard')"
+                                :active="request()->routeIs('orcamento.dashboard')">
+                                {{ __('Cancelamentos') }}
+                            </x-dropdown-link>
+
+
+                        </x-slot>
+                    </x-dropdown>
                 </div>
             </div>
 
@@ -134,10 +191,11 @@
         <div class="pb-5 text-xl">
             <x-side-link :href="route('orcamento.reports')" :active="request()->routeIs('orcamento.reports')"
                 class="w-full">
-                <x-icon name="presentation-chart-bar" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                <x-icon name="presentation-chart-bar"
+                    class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span class="ml-3">Relatórios</span>
             </x-side-link>
         </div>
-    
+
     </x-slot>
 </x-drawer>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false])
     <select
     {{ $disabled ? 'disabled' : '' }}
-        {{ $attributes->merge(['class' => 'mt-1 block font-bold rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-50 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'])}}>
+        {{ $attributes->merge(['class' => 'mt-1 block font-bold rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-50 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'])}}>
         {{$option}}
     </select>
