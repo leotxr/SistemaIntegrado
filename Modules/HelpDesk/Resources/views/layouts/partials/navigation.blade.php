0f71d9e66e48973af728b3de8e0fcd462ef742bb
leotxr <leonardof15@hotmail.com>
2023-09-26 14:18:44 -0300

    item centralizado

--- Modules/HelpDesk/Resources/views/layouts/partials/navigation.blade.php
+++ Modules/HelpDesk/Resources/views/layouts/partials/navigation.blade.php
@@ -82,7 +82,7 @@
     <div id="logo-sidebar"
         class="fixed top-0 left-0 z-40 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
         aria-label="Sidebar" :class="expanded ? 'sm:w-64' : 'sm:w-14'" x-transition>
-        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800" @mouseover="expanded = true"
+        <div class="h-full px-2 pb-4 overflow-y-auto bg-white dark:bg-gray-800" @mouseover="expanded = true"
             @mouseleave="expanded = false">
             <ul class="space-y-2 font-medium">
                 <li>
