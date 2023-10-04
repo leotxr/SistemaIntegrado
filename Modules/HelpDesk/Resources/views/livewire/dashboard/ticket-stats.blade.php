cb084486a0273c42bdec62785550fde836ef4c2f
leotxr <leonardof15@hotmail.com>
2023-10-04 15:39:51 -0300

    oldstyle

--- Modules/HelpDesk/Resources/views/livewire/dashboard/ticket-stats.blade.php
+++ Modules/HelpDesk/Resources/views/livewire/dashboard/ticket-stats.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xs:grid-cols-1 gap-3">
+<div class="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xs:grid-cols-1">
     <div class="px-4 py-6 bg-white rounded-lg shadow-md dark:bg-gray-800 ">
         <x-title>Tempo médio de atendimento</x-title>
         <span class="text-xs font-light text-gray-500">Tempo médio entre o início e final do atendimento</span>
@@ -23,16 +23,16 @@
         <x-title>Tempo médio de espera</x-title>
         <span class="text-xs font-light text-gray-500">Tempo médio de espera até o início do atendimento</span>
         <div class="grid content-center h-48 text-center">
-            <x-title class="text-5xl font-extrabold">@if(empty($tme_hoje)) 00:00:00 @else{{gmdate("H:i:s", $tme_hoje)}} @endif</x-title>
+            <x-title class="text-5xl font-extrabold oldstyle-nums">@if(empty($tme_hoje)) 00:00:00 @else{{gmdate("H:i:s", $tme_hoje)}} @endif</x-title>
             <span class="text-gray-400">Hoje</span>
             <div class="border-t">
                 <div class="grid grid-cols-2 gap-2 border-r">
                     <div>
-                        <x-title class="text-3xl font-bold">@if(empty($tme_ontem)) 00:00:00 @else{{gmdate("H:i:s", $tme_ontem)}} @endif </x-title>
+                        <x-title class="text-3xl font-bold oldstyle-nums">@if(empty($tme_ontem)) 00:00:00 @else{{gmdate("H:i:s", $tme_ontem)}} @endif </x-title>
                         <span class="text-gray-400">Ontem</span>
                     </div>
                     <div>
-                        <x-title class="text-3xl font-bold">@if(empty($tme_7d)) 00:00:00 @else{{gmdate("H:i:s", $tme_7d)}} @endif </x-title>
+                        <x-title class="text-3xl font-bold oldstyle-nums">@if(empty($tme_7d)) 00:00:00 @else{{gmdate("H:i:s", $tme_7d)}} @endif </x-title>
                         <span class="text-gray-400">7 dias</span>
                     </div>
                 </div>
