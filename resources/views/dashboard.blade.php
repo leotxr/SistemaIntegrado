9113828515e9a1f082425311ed58e28350e02c11
leotxr <leonardof15@hotmail.com>
2023-10-06 15:44:06 -0300

    teste server

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,13 @@
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+
+                    @php
+                    $IP = $_SERVER['REMOTE_ADDR']; 
+
+                    // Displaying the address
+                    echo "Client's IP address is: $IP"; 
+                    @endphp
                 </div>
             </div>
         </div>
