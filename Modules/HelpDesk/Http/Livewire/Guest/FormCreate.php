97805aa5a41e35f965315fa7eda7a5f2c47fe84b
leotxr <leonardof15@hotmail.com>
2023-10-11 10:43:22 -0300

    disparo de notificacao e evento

--- Modules/HelpDesk/Http/Livewire/Guest/FormCreate.php
+++ Modules/HelpDesk/Http/Livewire/Guest/FormCreate.php
@@ -82,8 +82,8 @@ class FormCreate extends Component
         };
         $this->saving->save();
 
-        //TicketCreated::dispatch(Auth::user(), $this->saving);
-        //Notification::send($users, new NotifyTicketCreated(Auth::user(), $this->saving));
+        TicketCreated::dispatch(Auth::user(), $this->saving);
+        Notification::send($users, new NotifyTicketCreated(Auth::user(), $this->saving));
 
         return redirect()->to('/helpdesk/chamados')->with('message', 'Chamado criado com sucesso!');
     }
