6e48f2059d3d7dac62d80ae65d60f4b6761f7889
leotxr <leonardof15@hotmail.com>
2023-10-19 10:20:55 -0300

    dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,4 +55,10 @@
     setTimeout(function() {
         $('.alert').fadeOut('slow');
     }, 5000);
+
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark')
+    };
 </script>
