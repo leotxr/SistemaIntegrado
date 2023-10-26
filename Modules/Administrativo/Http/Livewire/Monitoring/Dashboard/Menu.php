05f17d7b564962e46b3c35da72544fb0cb671109
leotxr <leonardof15@hotmail.com>
2023-10-26 17:45:29 -0300

    opcoes monitoramento

---
+++ Modules/Administrativo/Http/Livewire/Monitoring/Dashboard/Menu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Administrativo\Http\Livewire\Monitoring\Dashboard;
+
+use Livewire\Component;
+
+class Menu extends Component
+{
+    public function render()
+    {
+        return view('administrativo::livewire.monitoring.dashboard.menu');
+    }
+}
---
+++ Modules/Administrativo/Resources/views/livewire/monitoring/dashboard/menu.blade.php
@@ -0,0 +1,16 @@
+<div class="grid sm:grid-cols-4 grid-cols-1 gap-3">
+    <div class="sm:col-span-2 col-span-1">
+        <a href="{{url('/administrativo/monitoramento')}}">
+            <div class="p-2 bg-white dark:bg-gray-800 text-center rounded-lg">
+                Painel de monitoramento Livros
+            </div>
+        </a>
+    </div>
+    <div class="sm:col-span-2 col-span-1">
+        <a href="{{url('/administrativo/recepcao/fila-de-espera')}}">
+            <div class="p-2 bg-white dark:bg-gray-800 text-center rounded-lg">
+                Fila de Espera Recepção
+            </div>
+        </a>
+    </div>
+</div>
