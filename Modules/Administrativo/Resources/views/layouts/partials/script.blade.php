773e61c3ef6578dce85e59f882e9b89ab40937c8
leotxr <leonardof15@hotmail.com>
2023-11-21 15:08:37 -0300

    chamada vite

--- Modules/Administrativo/Resources/views/layouts/partials/script.blade.php
+++ Modules/Administrativo/Resources/views/layouts/partials/script.blade.php
@@ -1,7 +1,7 @@
     <!-- Scripts -->
-@vite(['Resources/assets/css/app.css', 'Resources/assets/js/app.js'])
+{{--@vite(['Resources/assets/css/app.css', 'Resources/assets/js/app.js'])--}}
 
-    {{-- module_vite('build-estoque', 'Resources/assets/css/app.css') --}}
+    {{ module_vite('build-administrativo', 'Resources/assets/css/app.css', 'Resources/assets/js/app.js') }}
 
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
