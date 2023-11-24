ea5223206f6a08e18ebfb065fee29430ea440f08
leotxr <leonardof15@hotmail.com>
2023-11-24 14:27:45 -0300

    merge class no select e table menor

--- resources/views/components/select/option.blade.php
+++ resources/views/components/select/option.blade.php
@@ -1,2 +1,2 @@
-<option class="text-gray-900 dark:text-gray-100" value="{{$value}}">{{$slot}}
+<option {!! $attributes->merge(['class' => 'text-gray-900 dark:text-gray-100'])!!} value="{{$value}}">{{$slot}}
 </option>
--- resources/views/components/table/cell.blade.php
+++ resources/views/components/table/cell.blade.php
@@ -1,1 +1,1 @@
-<td {{$attributes->merge(['class' => 'px-6 py-4'])}}>{{$slot}}</td>
+<td {{$attributes->merge(['class' => 'px-6 py-2'])}}>{{$slot}}</td>
