d4d974fe0a144f2ead0d4cd62c3b8d33913ec73a
leotxr <leonardof15@hotmail.com>
2023-11-24 07:32:27 -0300

    indicadores de carinhas vermelhas da fila de espera

--- Modules/Administrativo/Http/Livewire/Monitoring/ReceptionQueue.php
+++ Modules/Administrativo/Http/Livewire/Monitoring/ReceptionQueue.php
@@ -11,8 +11,12 @@ class ReceptionQueue extends Component
     public $queue;
     public $waiting;
     public $served;
-    public $count_served;
+    public $green;
+    public $yellow;
+    public $red;
     public $today;
+
+    public $delays = [];
 
     protected $listeners = ['refreshQueue' => 'refreshMe'];
 
@@ -22,31 +26,51 @@ class ReceptionQueue extends Component
             ->table('TOTEM_FILAS_ESPERA')
             // ->join('USUARIOS', 'USUARIOS.USERID', '=', 'TOTEM_FILAS_ESPERA.CHAMADO')
             ->whereBetween('TOTEM_FILAS_ESPERA.DATA', [$date, $date])
-            ->select(DB::raw("TOTEM_FILAS_ESPERA.SENHA AS SENHA, TOTEM_FILAS_ESPERA.HORACHEGADA AS HORACHEGADA, TOTEM_FILAS_ESPERA.HORA_ATEND AS HORAATEND, TOTEM_FILAS_ESPERA.TIPOFILA AS TIPOFILA, TOTEM_FILAS_ESPERA.ATENDIDO AS ATENDIDO, (DATEDIFF(second,0,CONVERT (TIME, GETDATE())) - TOTEM_FILAS_ESPERA.HORACHEGADA) AS ATRASO"));
+            ->select(DB::raw("TOTEM_FILAS_ESPERA.SENHA AS SENHA,
+            TOTEM_FILAS_ESPERA.HORACHEGADA AS HORACHEGADA,
+            TOTEM_FILAS_ESPERA.HORA_ATEND AS HORAATEND,
+            TOTEM_FILAS_ESPERA.HORA_ATEND - TOTEM_FILAS_ESPERA.HORACHEGADA AS ATRASO_ATEND,
+            TOTEM_FILAS_ESPERA.TIPOFILA AS TIPOFILA,
+            TOTEM_FILAS_ESPERA.ATENDIDO AS ATENDIDO,
+            (DATEDIFF(second,0,CONVERT (TIME, GETDATE())) - TOTEM_FILAS_ESPERA.HORACHEGADA) AS ATRASO"));
     }
 
     public function setQueues()
     {
+
         $this->queue = $this->getWaitQueue($this->today)->get();
         $this->served = $this->getWaitQueue($this->today)->join('USUARIOS', 'USUARIOS.USERID', '=', 'TOTEM_FILAS_ESPERA.CHAMADO')->get();
         $this->waiting = $this->getWaitQueue($this->today)->where('TOTEM_FILAS_ESPERA.ATENDIDO', 'F')->get();
 
+        /*
+                $this->green = $this->served->where('ATRASO_ATEND', '<', 600)->count();
+                $this->yellow = $this->served->where('ATRASO_ATEND', '>=', 600)->where('ATRASO_ATEND', '<', 900)->count();
+                $this->red = $this->served->where('ATRASO_ATEND', '>=', 900)->count();
+        */
 
 
         //dd($this->queue, $this->served, $this->waiting);
     }
 
+    public function setDelays()
+    {
+        $this->delays[0] = $this->served->where('ATRASO_ATEND', '<', 600)->count();
+        $this->delays[1] = $this->served->where('ATRASO_ATEND', '>=', 600)->where('ATRASO_ATEND', '<', 900)->count();
+        $this->delays[2] = $this->served->where('ATRASO_ATEND', '>=', 900)->count();
+    }
 
     public function refreshMe()
     {
         $this->today = now()->format('Y-m-d');
         $this->setQueues();
+        $this->setDelays();
     }
 
     public function mount()
     {
         $this->today = now()->format('Y-m-d');
         $this->setQueues();
+        $this->setDelays();
     }
 
 
--- Modules/Administrativo/Resources/views/livewire/monitoring/partials/reception-stats.blade.php
+++ Modules/Administrativo/Resources/views/livewire/monitoring/partials/reception-stats.blade.php
@@ -1,6 +1,7 @@
 <div>
     <div
-        class="grid w-full grid-cols-2 gap-1 px-2 py-4 bg-white divide-x-2 shadow-sm dark:bg-gray-800 sm:grid-cols-10 divide-solid">
+        class="grid w-full grid-cols-2 gap-1 px-2 py-4 bg-white divide-x-2 shadow-sm dark:bg-gray-800 sm:grid-cols-11 divide-solid content-center">
+
         <div class="content-center col-span-2 row-span-2 p-4 text-center sm:col-span-2">
             <div>
                 <span class="font-bold text-gray-700 text-7xl dark:text-gray-50">{{$queue->count()}}</span>
@@ -9,8 +10,6 @@
                 <span class="text-xl text-gray-500 font-regular dark:text-gray-200">Total</span>
             </div>
         </div>
-
-
 
         <div class="grid col-span-2 grid-rows-2 gap-1 text-center divide-y-2 sm:col-span-4 divide-solid">
             <div class="row-span-1">
@@ -24,7 +23,8 @@
             <div class="grid grid-cols-2 row-span-1 divide-x-2 divide-solid">
                 <div>
                     <div>
-                        <span class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$waiting->whereIn('TIPOFILA', ['A', 'P'])->count()}}</span>
+                        <span
+                            class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$waiting->whereIn('TIPOFILA', ['A', 'P'])->count()}}</span>
                     </div>
                     <div>
                         <span class="text-gray-500 text-md font-regular dark:text-gray-200">Atendimento</span>
@@ -32,7 +32,8 @@
                 </div>
                 <div>
                     <div>
-                        <span class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$waiting->where('TIPOFILA', 'T')->count()}}</span>
+                        <span
+                            class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$waiting->where('TIPOFILA', 'T')->count()}}</span>
                     </div>
                     <div>
                         <span class="text-gray-500 text-md font-regular dark:text-gray-200">Marcação</span>
@@ -53,7 +54,8 @@
             <div class="grid grid-cols-2 row-span-1 divide-x-2 divide-solid">
                 <div>
                     <div>
-                        <span class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$served->whereIn('TIPOFILA', ['A', 'P'])->count()}}</span>
+                        <span
+                            class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$served->whereIn('TIPOFILA', ['A', 'P'])->count()}}</span>
                     </div>
                     <div>
                         <span class="text-gray-500 text-md font-regular dark:text-gray-200">Atendimento</span>
@@ -61,13 +63,38 @@
                 </div>
                 <div>
                     <div>
-                        <span class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$served->where('TIPOFILA', 'T')->count()}}</span>
+                        <span
+                            class="text-4xl font-bold text-gray-700 dark:text-gray-50">{{$served->where('TIPOFILA', 'T')->count()}}</span>
                     </div>
                     <div>
                         <span class="text-gray-500 text-md font-regular dark:text-gray-200">Marcação</span>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="grid col-span-2 grid-rows-3 gap-1 sm:col-span-1 divide-solid content-center px-2">
+
+            <div class="row-span-1">
+                <div class="flex items-center">
+                    <x-icon name='emoji-happy' class="w-12 h-12 text-green-500" solid></x-icon>
+                    <span class="text-2xl font-bold text-gray-700 dark:text-gray-50">{{$delays[0]}}</span>
+                </div>
+            </div>
+            <div class="row-span-1">
+                <div class="flex items-center">
+                    <x-icon name='emoji-neutral' class="w-12 h-12 text-yellow-300" solid></x-icon>
+                    <span class="text-2xl font-bold text-gray-700 dark:text-gray-50">{{$delays[1]}}</span>
+                </div>
+            </div>
+            <div class="row-span-1">
+                <div class="flex items-center">
+                    <x-icon name='emoji-sad' class="w-12 h-12 text-red-500" solid></x-icon>
+                    <span class="text-2xl font-bold text-gray-700 dark:text-gray-50">{{$delays[2]}}</span>
+                </div>
+            </div>
+
+
         </div>
 
     </div>
