134d03c132d165cff507de44400c34efd02b7233
leotxr <leonardof15@hotmail.com>
2023-11-29 08:33:36 -0300

    pasta build original

--- Modules/Gestao/vite.config.js
+++ Modules/Gestao/vite.config.js
@@ -6,14 +6,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     build: {
-        outDir: '../../public/build',
+        outDir: '../../public/build-gestao',
         emptyOutDir: true,
         manifest: true,
     },
     plugins: [
         laravel({
             publicDirectory: '../../public',
-            buildDirectory: 'build',
+            buildDirectory: 'build-gestao',
             input: [
                 __dirname + '/Resources/assets/css/app.css',
                 __dirname + '/Resources/assets/js/app.js'
