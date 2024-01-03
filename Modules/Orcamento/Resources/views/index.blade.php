5a97fcba2e9ed19e6e9b1366d4612323c6acb726
leotxr <leonardof15@hotmail.com>
2024-01-03 16:42:29 -0300

    nome do header

--- Modules/Orcamento/Resources/views/index.blade.php
+++ Modules/Orcamento/Resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('header')
 
 <h2 class="space-x-2 text-xl leading-tight text-gray-800 divide-x-2 dark:text-gray-200 divide">
-    <span class="font-light"> Orçamentos </span> <span class="px-2 font-bold">Início</span>
+    <span class="font-light"> Encaixes e Desistências </span> <span class="px-2 font-bold">Início</span>
 </h2>
 
 @endsection
