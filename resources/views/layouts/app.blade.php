f5898f9ed674fca4f15637bd25c4d9dbb9fc575a
leotxr <leonardof15@hotmail.com>
2024-02-08 07:49:30 -0300

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 <body class="font-sans antialiased">
 
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-    @include('layouts.navigation')
+    @livewire('layouts.navigation', ['name' => 'Sigma'])
     @can('ti')
         @include('layouts.sidebar')
     @endcan
