86f101b89273fe17336c2bd166d1b229fa4556ed
leotxr <leonardof15@hotmail.com>
2024-02-20 13:06:03 -0300

    rollback

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,8 +17,8 @@ window.Echo = new Echo({
     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: false,
-    //enabledTransports: ['ws', 'wss'],
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
 
 
