efa584783d16a7f25d09067c257c8343b6e6d21f
leotxr <leonardof15@hotmail.com>
2024-02-22 14:18:32 -0300

    hover tabela

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false])
     <select
     {{ $disabled ? 'disabled' : '' }}
-        {{ $attributes->merge(['class' => 'mt-1 block font-bold rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-50 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'])}}>
+        {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded-md shadow-sm'])}}>
         {{$option}}
     </select>
--- resources/views/components/table/row.blade.php
+++ resources/views/components/table/row.blade.php
@@ -1,3 +1,3 @@
-<tr {{$attributes->merge(['class' => 'bg-white border-b dark:bg-gray-900 dark:border-gray-700'])}}>
+<tr {{$attributes->merge(['class' => 'bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600'])}}>
     {{$slot}}
 </tr>
