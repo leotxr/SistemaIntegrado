ae97bc2a10023d189da36450fe74c286e17ca787
leotxr <leonardof15@hotmail.com>
2024-05-06 13:30:10 -0300

    status de autorização

--- Modules/Autorizacao/Traits/ProtocolTraits.php
+++ Modules/Autorizacao/Traits/ProtocolTraits.php
@@ -12,7 +12,8 @@ trait ProtocolTraits
 {
     public $dateTime;
 
-    public function inProcessing($protocol_id){
+    public function inProcessing($protocol_id)
+    {
         $this->dateTime = strtotime('+5 minutes', strtotime(date('Y-m-d H:i:s')));
         ProtocolInUse::create([
             'protocol_id' => $protocol_id,
@@ -21,42 +22,45 @@ trait ProtocolTraits
         ]);
     }
 
-    public function checkProtocol($protocol_id){
+    public function checkProtocol($protocol_id)
+    {
         $protocol = DB::table('protocol_in_uses')->where('protocol_id', $protocol_id)->get();
 
-        if($protocol->count() > 0)
+        if ($protocol->count() > 0)
             return true;
         else
             return false;
     }
 
-    public function expiredProtocol($protocol_id){
+    public function expiredProtocol($protocol_id)
+    {
 
     }
 
-    public function endByProcotol($protocol_id){
-        if($this->checkProtocol($protocol_id))
-        {
+    public function endByProcotol($protocol_id)
+    {
+        if ($this->checkProtocol($protocol_id)) {
             DB::table('protocol_in_uses')->where('protocol_id', $protocol_id)->delete();
             return true;
-        }
-        else
+        } else
             return false;
 
     }
 
-    public function getUser($protocol_id){
+    public function getUser($protocol_id)
+    {
         $user = ProtocolInUse::where('protocol_id', $protocol_id)->first();
         $user = User::find($user['user_id']);
 
-        if($user)
+        if ($user)
             return $user;
         else
             return false;
 
     }
 
-    public function endByUser($user_id){
+    public function endByUser($user_id)
+    {
         DB::table('protocol_in_uses')->where('user_id', $user_id)->delete();
         return true;
     }
@@ -81,7 +85,7 @@ trait ProtocolTraits
             ->join('PROCEDIMENTOS', 'PROCEDIMENTOS.PROCID', '=', 'VW_CL_AGENDA_PORTALAGENDA.PROCID')
             ->where('VW_AGENDA.HORREQID', '=', $search_key)
             ->whereNotNull('VW_AGENDA.CONVENIOID')
-            ->whereNotIn('VW_AGENDA.CONVENIOID', [22,647,614])
+            ->whereNotIn('VW_AGENDA.CONVENIOID', [22, 647, 614])
             ->select(DB::raw("VW_CL_AGENDA_PORTALAGENDA.HORREQID, FORMAT(VW_AGENDA.DATA, 'yyyy-MM-dd') AS DATA, VW_AGENDA.PACIENTEID, VW_AGENDA.NOMEPAC, VW_AGENDA.CONVDESC, VW_CL_AGENDA_PORTALAGENDA.NOME_PROCEDIMENTO, PROCEDIMENTOS.CODIGO, PROCEDIMENTOS.CODTUSS, VW_AGENDA.CONVENIOID"))
             ->get();
     }
@@ -101,10 +105,12 @@ trait ProtocolTraits
         $saving->exam_cod = $exam['exam_cod'] ?? NULL;
         $saving->updated_by = Auth::user()->id;
         $saving->save();
-        if(auth()->user()->user_group_id === 5)
-        $saving->events()->sync([5 => ['user_id' => auth()->user()->id, 'created_at' => now()]], true);
+        if (auth()->user()->user_group_id === 5)
+            $saving->events()->sync([5 => ['user_id' => auth()->user()->id, 'created_at' => now()]]);
+        else
+            $saving->events()->sync([1 => ['user_id' => auth()->user()->id, 'created_at' => now()]]);
 
-        if($saving->save()) return true;
+        if ($saving->save()) return true;
         else return false;
     }
 }
