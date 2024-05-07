7ebab20bf8cc50a8665fcf8bfe4c287ad1f0e064
leotxr <leonardof15@hotmail.com>
2024-05-07 14:15:20 -0300

    relacionamento invoice

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Administrativo\Entities\FinancialInvoice;
 
 class Doctor extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'crm',
+        'external_id',
+        'user_id',
+        'treatment',
+        'active',
+        'observation'
+
+        ];
+
+    public function invoices()
+    {
+        return $this->hasMany(FinancialInvoice::class);
+    }
 }
