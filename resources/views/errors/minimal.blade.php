83fa24dbe1d53e5e94b05faadd2136cd78026ae4
leotxr <leonardof15@hotmail.com>
2024-05-22 14:24:52 -0300

    error page layout

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,10 +1,27 @@
-<x-guest-layout>
-    <div class="relative flex justify-center bg-gray-100 items-top dark:bg-gray-900 sm:items-center sm:pt-0">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="mobile-web-app-capable" content="yes">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 dark:bg-gray-900">
+<section>
+    <div class="relative flex min-h-screen justify-center  items-top sm:items-center sm:pt-0">
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="px-4 text-lg tracking-wider text-gray-500 border-gray-400 flex justify-center">
+                @yield('icon')
+            </div>
             <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                <div class="px-4 text-lg tracking-wider text-gray-500 border-r border-gray-400">
-                    @yield('icon')
-                </div>
 
                 <div class="px-4 text-lg tracking-wider text-gray-500 border-r border-gray-400">
                     @yield('code')
@@ -14,11 +31,21 @@
                     @yield('message')
                 </div>
             </div>
-            <div class="my-4 text-center">
+            <div class="my-4 text-center" x-data>
                 <a x-on:click="history.back()">
                     <x-secondary-button>Voltar à pagina anterior</x-secondary-button>
                 </a>
             </div>
         </div>
     </div>
-</x-guest-layout>
+</section>
+</body>
+</html>
+<script>
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
+</script>
+
