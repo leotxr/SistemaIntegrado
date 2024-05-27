1db1b7b44ffd70d00ccdae8e1820587f52accc11
leotxr <leonardof15@hotmail.com>
2024-05-27 14:17:00 -0300

    botão ver mais para chamados antigos mostrarem descrição

--- Modules/HelpDesk/Resources/views/livewire/guest/ticket-activity.blade.php
+++ Modules/HelpDesk/Resources/views/livewire/guest/ticket-activity.blade.php
@@ -1,7 +1,11 @@
 <div class="w-full">
-    <div class="p-2 border border-gray-300 dark:border-gray-700 rounded-lg space-y-2 bg-white dark:bg-gray-800" x-data="{message_form: false}">
+    <div class="p-2 border border-gray-300 dark:border-gray-700 rounded-lg space-y-2 bg-white dark:bg-gray-800"
+         x-data="{message_form: false}">
         <div class="w-full flex justify-end">
-            <x-secondary-button type="button" x-on:click="message_form = ! message_form"><span class="transition transform duration-300" :class="message_form ? 'rotate-45' : 'rotate-0'"><x-icon name="plus" class="w-4 h-4"></x-icon></span>Nova mensagem</x-secondary-button>
+            <x-secondary-button type="button" x-on:click="message_form = ! message_form"><span
+                        class="transition transform duration-300" :class="message_form ? 'rotate-45' : 'rotate-0'"><x-icon
+                            name="plus" class="w-4 h-4"></x-icon></span>Nova mensagem
+            </x-secondary-button>
         </div>
         <div x-show="message_form">
             @livewire('helpdesk::guest.ticket-message-form', ['ticket' => $ticket], key($ticket->id))
@@ -19,6 +23,20 @@
                     @endphp
                 </x-message-row>
             @endforeach
+        </div>
+        <div x-data="{ show: false }">
+            <div>
+                <a class="cursor-pointer" x-on:click="show = ! show">Ver mais</a>
+            </div>
+            <div x-show="show">
+                <x-message-row image="{{$ticket->TicketRequester->profile_img}}"
+                               user="{{$ticket->TicketRequester->name}}"
+                               time="{{$ticket->created_at->format('d/m/y H:i:s')}}">
+                    @php
+                        echo "$ticket->description\n";
+                    @endphp
+                </x-message-row>
+            </div>
         </div>
     </div>
 
