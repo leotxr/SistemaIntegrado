8181914afbb153e2b473b0f76bf95a2fb96cbf97
Leonardo <leonardof15@hotmail.com>
2025-03-07 18:20:19 -0300

    ajuste layout

--- Modules/Triagem/Resources/views/index.blade.php
+++ Modules/Triagem/Resources/views/index.blade.php
@@ -8,7 +8,7 @@
 
                     @livewire('triagem::cards.card-exames', ['path' => "storage/icons/RM.png", 'label' => 'RM Terreo', 'link' => 'filas.ressonancia'])
                     @livewire('triagem::cards.card-exames', ['path' => "storage/icons/RM.png", 'label' => 'RM Subsolo', 'link' => 'filas.ressonanciaSub'])
-                    @livewire('triagem::cards.card-exames', ['path' => "storage/icons/TC.png", 'label' => 'Tomografia', 'link' => 'filas.tomografia'])
+                    @livewire('triagem::cards.card-exames', ['path' => "storage/icons/TC.png", 'label' => 'TC', 'link' => 'filas.tomografia'])
 
                     
 
