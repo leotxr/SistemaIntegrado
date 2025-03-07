a3b7e6b9efb9ffdf6d3b60247199871100552a2e
Leonardo <leonardof15@hotmail.com>
2025-03-07 18:24:31 -0300

    card

--- Modules/Triagem/Resources/views/index.blade.php
+++ Modules/Triagem/Resources/views/index.blade.php
@@ -5,15 +5,27 @@
         <div class="text-center hero-content">
             <div class="max-w-md">
                 <div class="grid grid-cols-1 gap-8 sm:grid-cols-3 place-items-center">
+                    <!-- Card 1 -->
+                    @livewire('triagem::cards.card-exames', [
+                        'path' => 'storage/icons/RM.png',
+                        'label' => 'RM Terreo',
+                        'link' => 'filas.ressonancia',
+                    ])
 
-                    @livewire('triagem::cards.card-exames', ['path' => "storage/icons/RM.png", 'label' => 'RM Terreo', 'link' => 'filas.ressonancia'])
-                    @livewire('triagem::cards.card-exames', ['path' => "storage/icons/RM.png", 'label' => 'RM Subsolo', 'link' => 'filas.ressonanciaSub'])
-                    @livewire('triagem::cards.card-exames', ['path' => "storage/icons/TC.png", 'label' => 'TC', 'link' => 'filas.tomografia'])
+                    <!-- Card 2 -->
+                    @livewire('triagem::cards.card-exames', [
+                        'path' => 'storage/icons/RM.png',
+                        'label' => 'RM Subsolo',
+                        'link' => 'filas.ressonanciaSub',
+                    ])
 
-                    
-
+                    <!-- Card 3 -->
+                    @livewire('triagem::cards.card-exames', [
+                        'path' => 'storage/icons/TC.png',
+                        'label' => 'TC',
+                        'link' => 'filas.tomografia',
+                    ])
                 </div>
-
             </div>
         </div>
     </div>
