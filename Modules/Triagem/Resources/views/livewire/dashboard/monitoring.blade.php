7fb8ced998424220aa9d4b44ad5589195357cd35
Leonardo <leonardof15@hotmail.com>
2025-03-07 18:03:01 -0300

    aicionando subsolo

--- Modules/Triagem/Resources/views/livewire/dashboard/monitoring.blade.php
+++ Modules/Triagem/Resources/views/livewire/dashboard/monitoring.blade.php
@@ -40,6 +40,13 @@
                                         <label for="default-checkbox"
                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Ressonância</label>
                                     </div>
+                                    <div class="flex items-center mb-4">
+                                        <input wire:model.defer='sec' id="default-checkbox" type="checkbox" value="52"
+                                               name="sec[]"
+                                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                        <label for="default-checkbox"
+                                               class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Ressonância Subsolo</label>
+                                    </div>
                                     <div class="flex items-center">
                                         <input wire:model='sec' id="checked-checkbox" type="checkbox" value="14"
                                                name="sec[]"
