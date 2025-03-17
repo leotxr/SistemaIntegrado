4b11f67515db54c9e9051cfcfd143d4094caf75a
Leonardo <leonardof15@hotmail.com>
2025-03-17 20:38:59 -0300

    retirando do livewire

--- Modules/HelpDesk/Resources/views/reports/extra-services-report.blade.php
+++ Modules/HelpDesk/Resources/views/reports/extra-services-report.blade.php
@@ -4,7 +4,7 @@
     <div class="max-w-full justify-items-center">
         <div class="overflow-hidden shadow-sm sm:rounded-lg">
             <div>
-                @livewire('helpdesk::reports.extra-services.extra-services-report')
+               
             </div>
         </div>
     </div>
