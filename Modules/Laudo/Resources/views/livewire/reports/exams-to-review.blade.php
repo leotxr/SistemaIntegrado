cc07c84ce6ae703bd18ef59d6673858d9df30dcb
Leonardo Teixeira <62628623+leotxr@users.noreply.github.com>
2025-09-16 08:35:11 -0300

    corrigindo nome paciente

--- Modules/Laudo/Resources/views/livewire/reports/exams-to-review.blade.php
+++ Modules/Laudo/Resources/views/livewire/reports/exams-to-review.blade.php
@@ -38,7 +38,7 @@
                         <x-table.cell>
                             {{$exame->PACIENTEID}}
                         </x-table.cell>
-                        <x-table.cell>{{$exame->PACIENTENOME}}</x-table.cell>
+                        <x-table.cell>{{$exame->NOME}}</x-table.cell>
                         <x-table.cell>{{$exame->EXAME}}</x-table.cell>
                         <x-table.cell>{{$exame->MEDICO}}</x-table.cell>
                         <x-table.cell>{{$exame->SETOR}}</x-table.cell>
